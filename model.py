a099d06953a0a51c3ab24133751467980b39a547
Akshita Sharma <itsakshitasharma23@gmail.com>
2026-02-02 19:48:49 +0530

    model

---
+++ model.py
@@ -0,0 +1,11 @@
+from monai.networks.nets import UNet
+
+def get_model():
+    return UNet(
+        spatial_dims=3,
+        in_channels=1,
+        out_channels=2,
+        channels=(16, 32, 64, 128, 256),
+        strides=(2, 2, 2, 2),
+        num_res_units=2
+    )
